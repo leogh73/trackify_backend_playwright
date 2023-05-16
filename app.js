ae6b1c7fed216fde74e24147efbb6f82b5907a0d
Leonardo Cuevas <leocuevas73@gmail.com>
2023-05-16 14:31:11 -0300

    extra implementations

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
+import dotenv from 'dotenv/config';
 
 app.use(compression());
 app.setMaxListeners(20);
@@ -11,7 +12,23 @@ app.use(cors({ origin: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
 
-app.post('/clicoh/', async (req, res) => {
+import CryptoJS from 'crypto-js';
+// let vars = {};
+// let encrypted = CryptoJS.AES.encrypt(
+// 	JSON.stringify(vars),
+// 	process.env.SERVICE_ENCRYPTION_KEY,
+// ).toString();
+// console.log(encrypted);
+
+let bytes = CryptoJS.AES.decrypt(
+	'U2FsdGVkX18oEib/PFo9J257Ji9Mt4ARWteC2RT4forftvyuCmgNFmtVETnLk5DWo+GpUCStDlrCoGdUKUp9C74TSWjZas49W3KdV45Pqd09CNXymIio/5ZhoStaRjLJhERrlFYy+uZp6yVTtgfD/Bvi1OLsdbpfoyVxovlDjZ0OIq1iD8igydKcDeKTEgDjw1IUv8JWdNPwdZKKv5RLZ2bIVJdMJHE1LlK4/+jsriMxcIICn0jvzfN06YM1uKFv8FjG6zOdiLLTOl1thgQwXUY8yNbdjdqzKNW49dQWIDHZ6TX1yyTmj5u37p2Vtiffnk1j+CZhhluQ9lizw8JA/XuGC8ZryNq+/VNAEKfutjk=',
+	process.env.SERVICE_ENCRYPTION_KEY,
+);
+let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+
+import playwright from 'playwright-aws-lambda';
+
+app.post('/clicoh', async (req, res) => {
 	const { code } = req.body;
 
 	try {
@@ -19,7 +36,7 @@ app.post('/clicoh/', async (req, res) => {
 		const context = await browser.newContext();
 		const page = await context.newPage();
 
-		await page.goto(`${vars.CLICOH_API_URL1}`, {
+		await page.goto(`${decryptedData.CLICOH_API_URL1}`, {
 			waitUntil: 'load',
 		});
 		await page.type("input[name='codigo']", `${code}`);
@@ -27,7 +44,8 @@ app.post('/clicoh/', async (req, res) => {
 			await Promise.all([
 				page.waitForResponse(
 					(response) =>
-						response.url() === `${vars.CLICOH_API_URL2}` && response.request().method() === 'POST',
+						response.url().includes(`${decryptedData.CLICOH_API_URL2}`) &&
+						response.request().method() !== 'OPTIONS',
 				),
 				page.click('.fa.fa-search'),
 			])
@@ -40,7 +58,7 @@ app.post('/clicoh/', async (req, res) => {
 	}
 });
 
-app.post('/renaper/', async (req, res) => {
+app.post('/renaper', async (req, res) => {
 	const { code } = req.body;
 
 	try {
@@ -48,7 +66,7 @@ app.post('/renaper/', async (req, res) => {
 		const context = await browser.newContext();
 		const page = await context.newPage();
 
-		await page.goto(`${vars.RENAPER_API_URL1}`, {
+		await page.goto(`${decryptedData.RENAPER_API_URL1}`, {
 			waitUntil: 'load',
 		});
 
@@ -56,7 +74,7 @@ app.post('/renaper/', async (req, res) => {
 			new Promise((resolve, reject) => {
 				setTimeout(() => {
 					reject('FUNCTION TIMEOUT');
-				}, 9500);
+				}, 9000);
 			});
 
 		const fetchData = async () => {
@@ -64,7 +82,7 @@ app.post('/renaper/', async (req, res) => {
 			let response = await (
 				await Promise.all([
 					page.waitForResponse(
-						(res) => res.url() === `${vars.RENAPER_API_URL2}` && res.status() === 200,
+						(res) => res.url() === `${decryptedData.RENAPER_API_URL2}` && res.status() === 200,
 					),
 					page.click('#btn-consultar'),
 				])
