ed70fa56cf7c1836fa69cb8cd35c818909dfafe9
Leonardo Cuevas <leocuevas73@gmail.com>
2024-04-09 19:48:48 -0300

    via cargo fix attempt 3

--- src/services/via_cargo.js
+++ src/services/via_cargo.js
@@ -3,13 +3,10 @@ import vars from '../modules/crypto-js.js';
 const check = async (page, code) => {
 	return await (
 		await Promise.all([
-			page.goto(`${vars.VIA_CARGO_API_URL1}${code}`),
-			page.waitForSelector(
-				'#root > div > section > section > div.details-container > div.content > div.detail-info',
-			),
+			page.goto(`${vars.VIA_CARGO_API_URL1}${code}`, { waitUntil: 'load' }),
 			page.waitForResponse((res) => res.url().startsWith(`${vars.VIA_CARGO_API_URL2}`)),
 		])
-	)[2].json();
+	)[1].json();
 };
 
 export default { check };
