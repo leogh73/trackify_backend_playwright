f7f7a1bf85c71161072b72be4e24a926454c917b
Leonardo Cuevas <leocuevas73@gmail.com>
2024-04-09 19:42:21 -0300

    via cargo fix attempt 2

--- src/services/via_cargo.js
+++ src/services/via_cargo.js
@@ -4,9 +4,12 @@ const check = async (page, code) => {
 	return await (
 		await Promise.all([
 			page.goto(`${vars.VIA_CARGO_API_URL1}${code}`),
+			page.waitForSelector(
+				'#root > div > section > section > div.details-container > div.content > div.detail-info',
+			),
 			page.waitForResponse((res) => res.url().startsWith(`${vars.VIA_CARGO_API_URL2}`)),
 		])
-	)[1].json();
+	)[2].json();
 };
 
 export default { check };
